f8f32ab1b17a424b3a2f48fd2170248f21ae76a5
Jens Glander <jens.glander@sap.com>
2024-08-14 14:51:47 +0200

    add missing btp provider in step2

--- missions/live/mission_4356/step2/provider.tf
+++ missions/live/mission_4356/step2/provider.tf
@@ -1,8 +1,25 @@
 terraform {
   required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = "1.5.0"
+    }
+  }
+}
+
+######################################################################
+# Configure BTP provider
+######################################################################
+provider "btp" {
+  cli_server_url = var.cli_server_url
+  globalaccount  = var.globalaccount
+}
+
+terraform {
+  required_providers {
     cloudfoundry = {
       source  = "SAP/cloudfoundry"
-      version = "0.2.1-beta"
+      version = "1.0.0-rc1"
     }
   }
 }
@@ -11,7 +28,6 @@ terraform {
 # Configure CF provider
 ######################################################################
 provider "cloudfoundry" {
-    # resolve API URL from environment instance
-    api_url = var.cf_api_url
-    #origin = var.custom_idp_origin
+  # resolve API URL from environment instance
+  api_url = var.cf_api_url
 }
