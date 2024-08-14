ba87a210fa880091541d44392902278473b210f4
Jens Glander <jens.glander@sap.com>
2024-08-14 13:48:03 +0200

    exclude logged in user from cf user lists

--- missions/live/mission_4356/step2/main.tf
+++ missions/live/mission_4356/step2/main.tf
@@ -1,3 +1,14 @@
+data "btp_whoami" me {}
+
+locals {
+
+  cf_org_admins = setsubtract(toset(var.cf_org_admins), [data.btp_whoami.me.email])
+  cf_org_users = setsubtract(toset(var.cf_org_users), [data.btp_whoami.me.email])
+
+  cf_space_developers = setsubtract(toset(var.cf_space_developers), [data.btp_whoami.me.email])
+  cf_space_managers = setsubtract(toset(var.cf_space_managers), [data.btp_whoami.me.email])
+}
+
 ######################################################################
 # Create space using CF provider
 ######################################################################
@@ -11,7 +22,7 @@ resource "cloudfoundry_space" "dev" {
 ######################################################################
 # Define Org User role
 resource "cloudfoundry_org_role" "organization_user" {
-  for_each = toset(var.cf_org_users)
+  for_each = toset(local.cf_org_users)
   username = each.value
   type     = "organization_user"
   org      = var.cf_org_id
@@ -19,7 +30,7 @@ resource "cloudfoundry_org_role" "organization_user" {
 }
 # Define Org Manager role
 resource "cloudfoundry_org_role" "organization_manager" {
-  for_each   = toset(var.cf_org_admins)
+  for_each   = toset(local.cf_org_admins)
   username   = each.value
   type       = "organization_manager"
   org        = var.cf_org_id
@@ -29,7 +40,7 @@ resource "cloudfoundry_org_role" "organization_manager" {
 
 
 resource "cloudfoundry_space_role" "space_developer" {
-  for_each   = toset(var.cf_space_developers)
+  for_each   = toset(local.cf_space_developers)
   username   = each.value
   type       = "space_developer"
   space      = cloudfoundry_space.dev.id
@@ -38,7 +49,7 @@ resource "cloudfoundry_space_role" "space_developer" {
 }
 
 resource "cloudfoundry_space_role" "space_manager" {
-  for_each   = toset(var.cf_space_managers)
+  for_each   = toset(local.cf_space_managers)
   username   = each.value
   type       = "space_manager"
   space      = cloudfoundry_space.dev.id
