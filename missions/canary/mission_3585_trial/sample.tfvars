f7604638de96de1ebbcf4a57a2c1486c8bf47b17
Jens Glander <jens.glander@sap.com>
2024-08-15 17:20:47 +0200

    3585 trial canary

---
+++ missions/canary/mission_3585_trial/sample.tfvars
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------------------------------
+# Provider configuration
+# ------------------------------------------------------------------------------------------------------
+# Your global account subdomain
+#globalaccount  = "xxxxxxxx-xxxxxxx-xxxxxxx-xxxxxxxx-xxxxxx"
+
+#globalaccount  = "982b7367-5617-4968-b601-fa2ec71bf2f7"
+#globalaccount  = "5c465995trial-ga" // jglvod
+
+#globalaccount  = "775253e0-3e95-4cde-b005-cdc3688cdc2c"
+globalaccount   = "d63f0404trial-ga"
+
+# The CLI server URL (needs to be set to null if you are using the default CLI server)
+#cli_server_url = null
+cli_server_url  = "https://canary.cli.btp.int.sap/"
+
+# Region for your subaccount
+#region = "us10"
+region  = "eu10-canary"
+
+# Name of your sub account
+subaccount_name = "mission-3585-can-tf-jglvod-0108b"
+
+
+#subaccount_id   = "a2ce0d2f-e950-45cd-af98-1ec707126fd3"
+subaccount_id   = "b3411096-592c-4f8c-9c1d-a5ad7e7d5386"
+
+# ------------------------------------------------------------------------------------------------------
+# USER ROLES
+# ------------------------------------------------------------------------------------------------------
+subaccount_admins = ["jens.glander@sap.com"]
+
+launchpad_admins = ["jens.glander@vodafone.de", "jens.glander@sap.com"]
+
+bas_admins     = ["jens.glander@vodafone.de", "jens.glander@sap.com"]
+bas_developers = ["jens.glander@vodafone.de", "jens.glander@sap.com"]
---
+++ missions/canary/mission_3585_trial/variables.tf
@@ -0,0 +1,85 @@
+
+variable "use_optional_resources" {
+  type        = bool
+  description = "With this parameter it is possible to decide if QAS TF script runs with or w/o all optional resources"
+  default     = false
+}
+
+variable "globalaccount" {
+  type        = string
+  description = "The globalaccount subdomain where the sub account shall be created."
+}
+
+variable "subaccount_name" {
+  type        = string
+  description = "The subaccount name."
+  default     = "My SAP DC mission subaccount."
+}
+
+variable "subaccount_id" {
+  type        = string
+  description = "The subaccount ID."
+  default     = ""
+}
+
+variable "cli_server_url" {
+  type        = string
+  description = "The BTP CLI server URL."
+  default     = "https://cli.btp.cloud.sap"
+}
+
+variable "region" {
+  type        = string
+  description = "The region where the subaccount shall be created in."
+  default     = "us10"
+}
+
+variable "subaccount_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to each subaccount as emergency administrators."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.subaccount_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.subaccount_admins)
+    error_message = "Please enter a valid email address for the subaccount admins."
+  }
+}
+
+variable "launchpad_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are Launchpad Admins."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.launchpad_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.launchpad_admins)
+    error_message = "Please enter a valid email address."
+  }
+}
+
+variable "bas_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are admins for SAP Business Application Studio."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.bas_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.bas_admins)
+    error_message = "Please enter a valid email address."
+  }
+}
+
+variable "bas_developers" {
+  type        = list(string)
+  description = "Defines the colleagues who are developers for SAP Business Application Studio."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.bas_developers : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.bas_developers)
+    error_message = "Please enter a valid email address."
+  }
+}
+
+variable "custom_idp" {
+  type        = string
+  description = "The custom identity provider for the subaccount."
+  default     = "sap.ids"
+}
