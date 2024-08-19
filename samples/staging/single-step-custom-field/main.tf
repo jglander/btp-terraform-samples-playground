cfc4bf839cac407e4197700ca0d79c16e2356eea
Jens Glander <jens.glander@sap.com>
2024-08-19 11:31:06 +0200

    sample for custom input field used in TF

---
+++ samples/staging/single-step-custom-field/main.tf
@@ -0,0 +1,42 @@
+###############################################################################################
+# Setup subaccount domain and the CF org (to ensure uniqueness in BTP global account)
+###############################################################################################
+resource "random_uuid" "uuid" {}
+
+locals {
+  random_uuid               = random_uuid.uuid.result
+  project_subaccount_domain = "buildapps${local.random_uuid}"
+}
+
+data "btp_globalaccount" "this" {}
+
+###############################################################################################
+# Creation of subaccount
+###############################################################################################
+resource "btp_subaccount" "create_subaccount" {
+  count = var.subaccount_id == "" ? 1 : 0
+
+  name      = "${var.custom_name}-${var.subaccount_name}"
+  subdomain = local.project_subaccount_domain
+  region    = lower(var.region)
+  usage     = "USED_FOR_PRODUCTION"
+}
+
+data "btp_subaccount" "project" {
+  id = var.subaccount_id != "" ? var.subaccount_id : btp_subaccount.create_subaccount[0].id
+}
+
+###############################################################################################
+# Prepare CF
+###############################################################################################
+# Entitle subaccount for usage of cf runtime
+resource "btp_subaccount_entitlement" "prepare_cf" {
+  
+  // global accounts which use consumption-based commercial model are implicitly entitled, see https://help.sap.com/docs/btp/sap-business-technology-platform/what-is-consumption-based-commercial-model
+  count = data.btp_globalaccount.this.consumption_based ? 0 : 1
+  
+  subaccount_id = data.btp_subaccount.project.id
+  service_name  = local.service_name__cf_runtime
+  plan_name     = var.service_plan__cf_runtime
+  amount        = 1
+}
---
+++ samples/staging/single-step-custom-field/variables.tf
@@ -0,0 +1,50 @@
+variable "globalaccount" {
+  type        = string
+  description = "The globalaccount subdomain where the sub account shall be created."
+}
+
+variable "custom_name" {
+  type        = string
+  description = "required custom name"
+}
+
+variable "subaccount_name" {
+  type        = string
+  description = "The subaccount name."
+  default     = "my new subacc name"
+}
+
+variable "subaccount_id" {
+  type        = string
+  description = "The subaccount ID."
+  default     = ""
+}
+ 
+variable "cli_server_url" {
+  type        = string
+  description = "The BTP CLI server URL."
+  default     = "https://cli.btp.cloud.sap/"
+}
+ 
+variable "custom_idp" {
+  type        = string
+  description = "Defines the custom IDP to be used for the subaccount"
+  default = "" 
+}
+ 
+variable "region" {
+  type        = string
+  description = "The region where the sub account shall be created in."
+  default     = "us10"
+}
+
+variable "service_plan__cf_runtime" {
+  type        = string
+  description = "The plan for cf runtime"
+  default     = "MEMORY"
+  validation {
+    condition     = contains(["MEMORY"], var.service_plan__cf_runtime)
+    error_message = "Invalid value for service_plan__cf_runtime. Only 'MEMORY' is allowed."
+  }
+}
+
