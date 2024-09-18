713c8ab577f60f8e26324ee648f95afe7278cff4
Jens Glander <jens.glander@sap.com>
2024-09-18 17:28:20 +0200

    Added kyma dashboard url

--- missions/canary/mission_3252/outputs.tf
+++ missions/canary/mission_3252/outputs.tf
@@ -1,3 +1,8 @@
 output "subaccount_id" {
   value = data.btp_subaccount.dc_mission.id
 }
+
+output "kyma_env_instance_dashboard_url" {
+  value       = btp_subaccount_environment_instance.kyma.dashboard_url
+  description = "The URL of the kyma environment dashboard."
+}
