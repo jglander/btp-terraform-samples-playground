e940ec02e0f35025a6a4aa30ae397d34f9a131b9
Jens Glander <jens.glander@sap.com>
2024-09-26 21:34:03 +0200

    4356: fix integration url

---
+++ missions/live/mission_4356_crazy2/step2/outputs.tf
@@ -0,0 +1,4 @@
+output "integrationsuite_subscription_url" {
+  value       = btp_subaccount_subscription.integrationsuite.subscription_url
+  description = "SAP Integration Suite subscription URL."
+}
